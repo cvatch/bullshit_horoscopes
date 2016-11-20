8fa410a30d5c8088e6bba9e80a1a93306cb7df85
Robin Nemeth <nemeth.robin@gmail.com>
2016-11-20 16:59:24 +0100

    Fixed paths to be more universal

--- horoscope/predict.py
+++ horoscope/predict.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import random
+import os
+BASEDIR = os.path.dirname(os.path.realpath(__file__))
 
 def get_first_word(first_word_probabilities):
+    """Create first word of a sentence"""
     val = random.random()
     return first_word_probabilities.query('cumsum >= {}'.format(val)).iloc[0]['word1']
 
@@ -23,13 +26,35 @@ def create_sentence(first_word_probabilities, bigrams_probs):
 
 
 def create_horoscope(size, first_word_probabilities, bigrams_probs):
+    '''Acutally create the horoscope by adding n sentences
+
+    Arguments:
+        size (int): desired length of horoscope in sentences
+        first_word_probabilties
+    '''
     horoscope = ''
     for _ in range(size):
         horoscope += create_sentence(first_word_probabilities, bigrams_probs)
     return horoscope
 
 
-def load_frames(bigrams, first_words):
+def load_frames(
+        bigrams=os.path.join(BASEDIR, 'bigrams_probs.csv'),
+        first_words=os.path.join(BASEDIR, 'first_world_probabilities.csv')):
+    '''Load dataframes containing probabilities of first words
+    and all bigrams
+
+
+    generally, the bigrams df has columns 'word1', 'word2', 'probabiltiy', 'cumsum'
+    first_word_probabilties has columns 'word1', 'probabiltiy', 'cumsum'
+
+    Arguments (in vanilla setup):
+        bigrams (str): "bigram_probs.csv"
+        first_words (str): "first_world_probabilities.csv"
+
+    Retruns:
+        pandas dataframes
+    '''
     bigrams_probs = pd.read_csv(bigrams, index_col=0)
     first_word_probabilities = pd.read_csv(first_words, index_col=0)
     return bigrams_probs, first_word_probabilities
